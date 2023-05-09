31e26c092bd8900e425bf1432f77319d2a9925f2
rizwar <rizwar.adnanbyantara@gmail.com>
2023-05-09 11:39:18 +0700

    .

--- resources/views/listperiode.blade.php
+++ resources/views/listperiode.blade.php
@@ -26,8 +26,8 @@
                 @if ($d->status == 0)
                     <td><button class='text-success border border-2 border-success rounded bg-light p-3' id='aktif'>Aktif</button></td>
                 @elseif ($d->status == 1)
-                    <td><button class='text-danger border border-2 border-danger rounded bg-light p-3' id='tidak_aktif{{$d->id}}' value="{{$d->id}}">Tidak Aktif</button></td>
-                @endif              
+                    <td><button class='text-danger border border-2 border-danger rounded bg-light p-3' id='tidak_aktif' value="{{$d->id}}">Tidak Aktif</button></td>
+                @endif
                 <td><a href='{{url('/periode/'.$d->id)}}' class='btn btn-warning'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -50,7 +50,7 @@
             })
         })
 
-        $('#tidak_aktif1, #tidak_aktif2, #tidak_aktif3, #tidak_aktif4, #tidak_aktif5, #tidak_aktif6, #tidak_aktif7, #tidak_aktif8, #tidak_aktif9, #tidak_aktif10, #tidak_aktif11, #tidak_aktif12, #tidak_aktif13, #tidak_aktif14, #tidak_aktif15, #tidak_aktif16, #tidak_aktif17, #tidak_aktif18, #tidak_aktif19, #tidak_aktif20').on("click", function(){
+        $('button[id^="tidak_aktif"]').on("click", function(){
             var id = $(this).val();
             Swal.fire({
                 title: 'Ingin mengubah periode saat ini?',
@@ -69,9 +69,9 @@
                         var data = JSON.parse(response);
                         Swal.fire({
                             title: 'Berhasil Mengubah!',
-                            confirmButtonText: 'Ok', 
+                            confirmButtonText: 'Ok',
                             icon: 'success'
-                        }).then((resultt) =>{ 
+                        }).then((resultt) =>{
                             if(resultt.isConfirmed){
                                 location.reload();
                             }else{
